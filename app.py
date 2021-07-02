84969b4c740881022cf770e8026ea432eb2e5180
unknown <marshalmurimi@gmail.com>
2021-07-02 12:59:43 +0200

    Add all my files

---
+++ app.py
@@ -0,0 +1,38 @@
+#coding=utf-8
+from flask import Flask, render_template, request, jsonify
+import aiml
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('chat.html')
+
+@app.route("/ask", methods=['POST'])
+def main():
+    message = request.form['messageText']
+
+    kernel = aiml.Kernel()
+
+    if os.path.isfile("bot_brain.brn"):
+        kernel.bootstrap(brainFile = "bot_brain.brn")
+    else:
+        #kernel.bootstrap(learnFiles = os.path.abspath("aiml/std-startup.xml"), commands = "load aiml b")
+        kernel.learn("std-startup.xml")
+        kernel.respond("load aiml b")
+        kernel.saveBrain("bot_brain.brn")
+
+    # kernel now ready for use
+    while True:
+        if message == "quit":
+            exit()
+        elif message == "save":
+            kernel.saveBrain("bot_brain.brn")
+        else:
+            bot_response = kernel.respond(message)
+               # print bot_response
+            return jsonify({'status':'OK','answer':bot_response})
+
+if __name__ == "__main__":
+    app.run(debug=True, host='127.0.0.1')
